9ca3569e601ccf75bac1d38a8d433b3f43840cb1
Nicolas Alliaume <nicolasalliaume@gmail.com>
2014-07-12 20:39:41 -0300

    Improved Doc
    Improved doc on the .py file

--- ONPyTimer.py
+++ ONPyTimer.py
@@ -2,6 +2,29 @@
 from time import *
 
 class ONPyTimer(object):
+	'''	
+		ONPyTimer allows to monitor time difference between the lines of a
+		Python script in a stopwatch-like manner.
+
+		=== Usage: =====================
+
+		from ONPyTimer import *
+
+		ONPyTimer.check(flag='A')   # will show current time and flag name
+		...
+		ONPyTimer.check(flags=['A']) # will show time diff from flag 'A'
+		...
+		ONPyTimer.check(flag='B', flags=['A']) # will show new flag name and time diff
+		... 								   # from flag 'A'
+		...
+		ONPyTimer.check(flags=['A', 'B']) # will show time diff from flags 'A' and 'B'
+
+		================================
+
+		Author: Nicolas Alliaume - ON Lab
+		GitHub: http://github.com/nicolasalliaume/ONPyTimer
+		@ 2014
+	'''
 
 	__measures = {}
 
